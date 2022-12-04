5a7c0839a815c3fb39645d94259cf31876be77ab
Amit Chaudhary <amitchaudhary0539@gmail.com>
2022-12-04 22:09:23 +0530

    fix static route

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -149,8 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'expensewebsite/static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 # Default primary key field type
